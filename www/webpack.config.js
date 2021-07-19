a0c7e6ca20e10c204fb54a2037777436bc59d71c
Karthik <karthik1705.n@gmail.com>
2021-07-19 12:12:23 +0530

    misc(www): final output filename modified

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,9 +1,6 @@
-const path = require('path');
-
 module.exports = {
   entry: "./bootstrap.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bootstrap.js",
+    filename: "output.js"
   }
 };
